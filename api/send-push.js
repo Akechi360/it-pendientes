f9128b25b9e2e46ae57097f54db60e357914405b
Akechi360 <ubuntusptech360@gmail.com>
2026-08-20 15:06:54 -0400

    fix: resolve syntax error in Vercel Serverless Function breaking OneSignal auth header interpolation

--- api/send-push.js
+++ api/send-push.js
@@ -1,4 +1,4 @@
-﻿export default async function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed' });
   }
@@ -21,7 +21,7 @@
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': \Key \\,
+        'Authorization': 'Key ' + restApiKey,
         'Accept': 'application/json'
       },
       body: JSON.stringify({
